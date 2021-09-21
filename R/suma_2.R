e52ed12643ccfc604854a0021f70abb2bfe14e4a
Daniel <danimonzon@gmail.com>
2021-09-21 17:58:43 +0200

    Envío Inicial
    Envío inicial de la libreria

---
+++ R/suma_2.R
@@ -0,0 +1,12 @@
+#' @title suma_2
+#' @description Suma 2 numeros
+#' @param a primer numero
+#' @param b segundo numero
+#' @return la suma de a y b
+#' @examples
+#' suma_2(5,4)
+#' suma_2(-7,18)
+
+suma_2 <- function(a, b) {
+  sum(a + b)
+}
---
+++ R/suma_3.R
@@ -0,0 +1,13 @@
+#' @title suma_3
+#' @description Suma 3 numeros
+#' @param a primer numero
+#' @param b segundo numero
+#' @param c tercer numero
+#' @return la suma de a, b y c
+#' @examples
+#' suma_3(5,4,7)
+#' suma_3(8,-4,7)
+
+suma_3 <- function(a, b, c) {
+  sum(a + b + c)
+}
